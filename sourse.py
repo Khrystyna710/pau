d045b2065423c1fcb71e78de40c344933e489397
Paul1 <kovalchuk412@gmail.com>
2020-08-28 18:21:01 +0300

    add end program

--- sourse.py
+++ sourse.py
@@ -4,3 +4,8 @@ while True:
     numberUser = int(input())
     if numberUser == randomNumber:
         print("You crazy!!! WOW")
+        break
+    elif numberUser > 1000:
+        print("You tutu?")
+    else:
+        print("nope")
